56181d5aef7a57d49b767736e93e9d01c64cd4f6
tpribic <tomi.pribic@gmail.com>
2020-08-08 19:15:12 +0200

    Add entities and migration

---
+++ src/Issue/Infrastracture/Doctrine/Main/Entity/Comment.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Issue\Infrastracture\Doctrine\Main\Entity;
+
+use App\Issue\Infrastracture\Doctrine\Main\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+final class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Issue::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $issue_id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user_id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getIssueId(): ?Issue
+    {
+        return $this->issue_id;
+    }
+
+    public function setIssueId(?Issue $issue_id): self
+    {
+        $this->issue_id = $issue_id;
+
+        return $this;
+    }
+
+    public function getUserId(): ?User
+    {
+        return $this->user_id;
+    }
+
+    public function setUserId(?User $user_id): self
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
+}
